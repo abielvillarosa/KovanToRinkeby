e29edf81cb0c4a471f386d54c9ed6b011e4ae88c
Abiel Villarosa <john_abiel@yahoo.com>
2019-08-13 19:30:46 -0400

    Remove transfer

--- KovanToRinkeby.sol
+++ KovanToRinkeby.sol
@@ -7,7 +7,6 @@ contract KovanToRinkeby {
     event TransferToRinkeby(uint256 amount, address sender);
     
     function () external payable {
-        msg.sender.transfer(msg.value);
         emit TransferToRinkeby(msg.value, msg.sender);
     }
         
