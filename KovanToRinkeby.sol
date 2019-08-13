caab70a746d46ed9f48305002e4128f9a59df984
Abiel Villarosa <john_abiel@yahoo.com>
2019-08-13 19:19:50 -0400

    Add deployed contract

--- KovanToRinkeby.sol
+++ KovanToRinkeby.sol
@@ -2,7 +2,8 @@ pragma solidity ^0.5.1;
 
 contract KovanToRinkeby {
     
-    
+    //Contract Address: 0x94b4ba719ab4060be38a1c39b1562f61dee2386d
+
     event TransferToRinkeby(uint256 amount, address sender);
     
     function () external payable {
